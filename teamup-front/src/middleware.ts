4c03c4bdf38d5fb109f2ecf1d281acd0fde06a27
Nathaniel Josse <nathaniel.josse@gmail.com>
2025-09-05 12:54:17 +0200

    Fixes for production: fixed middleware

--- teamup-front/src/middleware.ts
+++ teamup-front/src/middleware.ts
@@ -28,9 +28,9 @@ export function middleware(request: NextRequest) {
         cspHeader = `
         default-src 'self';
         script-src 'self' 'nonce-${nonce}' https://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/ https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@*;
-        connect-src 'self' ${backendUrl} https://www.google.com https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@* https://{s}.tile.openstreetmap.org https://tile.openstreetmap.org https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org;
+        connect-src 'self' ${backendUrl} https://www.google.com https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@*  https://tile.openstreetmap.org https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org;
         style-src 'self' 'nonce-${nonce}' https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@*;
-        img-src 'self' blob: data: https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@* https://{s}.tile.openstreetmap.org https://tile.openstreetmap.org https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org;
+        img-src 'self' blob: data: https://unpkg.com/leaflet@* https://cdn.jsdelivr.net/npm/leaflet@* https://tile.openstreetmap.org https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org;
         frame-src https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/;
         object-src 'none';
         base-uri 'self';
